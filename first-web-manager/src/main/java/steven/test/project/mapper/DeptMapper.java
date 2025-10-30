322bdfd40046007bae38bdde3e12746dbd1d9cf4
zgj888 <2369394195@qq.com>
2025-10-30 17:57:53 +0800

    接口准备阶段

--- first-web-manager/src/main/java/steven/test/project/mapper/DeptMapper.java
+++ first-web-manager/src/main/java/steven/test/project/mapper/DeptMapper.java
@@ -1,8 +1,11 @@
 package steven.test.project.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import steven.test.project.zhao.Dept;
+
+import java.util.List;
 
 @Mapper
 public interface DeptMapper {
-
+    List<Dept> getTable();
 }
--- first-web-manager/src/test/java/steven/test/project/FirstWebManagerApplicationTests.java
+++ first-web-manager/src/test/java/steven/test/project/FirstWebManagerApplicationTests.java
@@ -1,13 +1,26 @@
 package steven.test.project;
 
+// import org.junit.jupiter.api.Disabled;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import steven.test.project.mapper.DeptMapper;
+import steven.test.project.zhao.Dept;
+
+import java.util.List;
 
 @SpringBootTest
 class FirstWebManagerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private DeptMapper deptMap;
+
+    @Test
+    void testGetTable() {
+        System.out.println("测试");
+        List<Dept> list = deptMap.getTable();
+        list.forEach(System.out::println);
+    }
 
 }
