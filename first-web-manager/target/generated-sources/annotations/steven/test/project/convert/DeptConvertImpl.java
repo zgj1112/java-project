59e7b95b2b8c314b78355c4329a09f254f4eb6e8
zgj888 <2369394195@qq.com>
2025-12-22 17:26:50 +0800

    合并

--- first-web-manager/target/generated-sources/annotations/steven/test/project/convert/DeptConvertImpl.java
+++ first-web-manager/target/generated-sources/annotations/steven/test/project/convert/DeptConvertImpl.java
@@ -8,22 +8,18 @@ import steven.test.project.vo.DeptRespVO;
 import steven.test.project.vo.DeptUpdateReqVO;
 import steven.test.project.zhao.Dept;
 
-@Generated(
-    value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-12-01T14:29:38+0800",
-    comments = "version: 1.6.0, compiler: javac, environment: Java 24.0.1 (Oracle Corporation)"
-)
+@Generated(value = "org.mapstruct.ap.MappingProcessor", date = "2025-12-22T17:22:09+0800", comments = "version: 1.6.0, compiler: Eclipse JDT (IDE) 3.44.0.v20251118-1623, environment: Java 21.0.9 (Eclipse Adoptium)")
 public class DeptConvertImpl implements DeptConvert {
 
     @Override
     public List<DeptRespVO> convertList(List<Dept> list) {
-        if ( list == null ) {
+        if (list == null) {
             return null;
         }
 
-        List<DeptRespVO> list1 = new ArrayList<DeptRespVO>( list.size() );
-        for ( Dept dept : list ) {
-            list1.add( deptToDeptRespVO( dept ) );
+        List<DeptRespVO> list1 = new ArrayList<DeptRespVO>(list.size());
+        for (Dept dept : list) {
+            list1.add(deptToDeptRespVO(dept));
         }
 
         return list1;
@@ -31,48 +27,48 @@ public class DeptConvertImpl implements DeptConvert {
 
     @Override
     public Dept convert(DeptCreateReqVO bean) {
-        if ( bean == null ) {
+        if (bean == null) {
             return null;
         }
 
         Dept dept = new Dept();
 
-        dept.setName( bean.getName() );
-        dept.setCount( bean.getCount() );
-        dept.setRemark( bean.getRemark() );
+        dept.setCount(bean.getCount());
+        dept.setName(bean.getName());
+        dept.setRemark(bean.getRemark());
 
         return dept;
     }
 
     @Override
     public Dept convert(DeptUpdateReqVO bean) {
-        if ( bean == null ) {
+        if (bean == null) {
             return null;
         }
 
         Dept dept = new Dept();
 
-        dept.setId( bean.getId() );
-        dept.setName( bean.getName() );
-        dept.setCount( bean.getCount() );
-        dept.setRemark( bean.getRemark() );
+        dept.setCount(bean.getCount());
+        dept.setId(bean.getId());
+        dept.setName(bean.getName());
+        dept.setRemark(bean.getRemark());
 
         return dept;
     }
 
     protected DeptRespVO deptToDeptRespVO(Dept dept) {
-        if ( dept == null ) {
+        if (dept == null) {
             return null;
         }
 
         DeptRespVO deptRespVO = new DeptRespVO();
 
-        deptRespVO.setId( dept.getId() );
-        deptRespVO.setName( dept.getName() );
-        deptRespVO.setCount( dept.getCount() );
-        deptRespVO.setRemark( dept.getRemark() );
-        deptRespVO.setCreateTime( dept.getCreateTime() );
-        deptRespVO.setUpdateTime( dept.getUpdateTime() );
+        deptRespVO.setCount(dept.getCount());
+        deptRespVO.setCreateTime(dept.getCreateTime());
+        deptRespVO.setId(dept.getId());
+        deptRespVO.setName(dept.getName());
+        deptRespVO.setRemark(dept.getRemark());
+        deptRespVO.setUpdateTime(dept.getUpdateTime());
 
         return deptRespVO;
     }
